888174a679c5b78f271ddcadaaac27676cb2616b
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-06-08 12:48:46 +0530

    Timer text is added.

--- Assets/Scripts/PlayerBulletController.cs
+++ Assets/Scripts/PlayerBulletController.cs
@@ -10,6 +10,7 @@ public class PlayerBulletController : MonoBehaviour
     public ScoreManager kills;
     [SerializeField] GameObject explosionPrefab;
     [SerializeField] GameObject asteroidPrefab;
+   
     #endregion
     
 
@@ -41,10 +42,11 @@ public class PlayerBulletController : MonoBehaviour
             PoolManager.Instance.Recycle(Constants.PLAYER_BULLET_PREFAB, this.gameObject);
             PoolManager.Instance.Recycle(Constants.ASTEROID_PREFAB, collision.gameObject);
             kills.ScoreCalculater(5);
-            Debug.Log("Asteroid sound");
+           
             Instantiate(asteroidPrefab, transform.position, Quaternion.identity);
-
-            collision.gameObject.transform.rotation = Quaternion.EulerRotation(collision.gameObject.transform.position.y - 0.05f, 0f, 0f);
+            
+            
+            //collision.gameObject.transform.rotation = Quaternion.EulerRotation(collision.gameObject.transform.position.y - 0.05f, 0f, 0f);
             // SOUNDS 
             // EXPLOSION PARTICAL EFFECT
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,11 +12,15 @@ public class PlayerController : MonoBehaviour
     public Transform bulletPosition;
     public const string TURN_ON_COROUTINE = "NextBullet";
     public AudioSource laserSound;
+    public float timer;
+    public Text timerText;
+
     #endregion
 
     #region PRIVATE VARIABLE
     private Rigidbody2D rb;
     private UIManager uiManager;
+   
     #endregion
 
     #region SINGLETON
@@ -58,6 +62,16 @@ public class PlayerController : MonoBehaviour
         InputHandler.OnPanHeld -= PositionOfShip;
         InputHandler.OnTouchAction -= ShootBullets;
     }
+    private void Update()
+    {
+        timer =  timer + Time.deltaTime;
+       
+        timerText.text =((int)timer).ToString();
+        if(timer > 60)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
     #endregion
 
     #region PUBLIC METHODS
@@ -80,11 +94,14 @@ public class PlayerController : MonoBehaviour
         Debug.Log("life" + lives);
         StartCoroutine(StartInvincibilityTimer(2.5f));
         uiManager.UpdateLives(lives);
+        
 
         if (lives <= 0)
         {
+
             Debug.Log("game over");
             SceneManager.LoadScene(3);
+         
 
 
         }
